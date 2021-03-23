fbeab80c02ddfb755c5f19a951268c5687c89882
artofmayhem <chef_tony_miller@yahoo.com>
2021-03-23 11:33:33 -1000

    initial package commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Title from './components/title';
+import Recipes from './components/recipes';
+
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css"
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="parallax-bg jumbotron" style={{minHeight: '100vh'}}>
+      <div className="container">
+       
+      <Title /> 
+      <Recipes /> 
+     </div>  
+     </div>
+
   );
 }
 
